24793ba0e96ee26d921348dbeaf598923cca1bfe
kusha <0xkrusha@gmail.com>
2022-09-06 03:43:04 +0700

    use component did update hook

---
+++ libs/usehooks/src/lib/useComponentDidUpdate.ts
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+
+export default function useComponentDidUpdate<T>(
+    callback: (prevProps: T) => void,
+    nextProps: T = {} as T,
+  ): void {
+    const didMount = useRef(false);
+    const prevProps = useRef<T>(nextProps);
+    useEffect(() => {
+      if (didMount.current) {
+        callback(prevProps.current);
+        prevProps.current = nextProps;
+      } else {
+        didMount.current = true;
+      }
+    });
+  }
