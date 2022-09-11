61b5dc16b88b7a7dab67c4bcacf69ac5b5650109
kusha <0xkrusha@gmail.com>
2022-09-11 15:56:36 +0700

    auth

---
+++ apps/fafty/src/auth/index.tsx
@@ -0,0 +1,77 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { HttpAgent } from '@dfinity/agent';
+
+import { Principal } from '@dfinity/principal';
+import internetIdentity, { WalletInterface } from '../utils/wallet/il';
+
+export interface AuthContext {
+  isShow: boolean;
+  showModal: (show: boolean) => void;
+
+  wallet?: WalletInterface;
+  principal?: Principal;
+  agent?: HttpAgent;
+
+  balance: bigint | null;
+
+  useInternetIdentity: () => void;
+
+  setPrincipal: (principal: Principal | undefined) => void;
+  setAgent: (agent: HttpAgent | undefined) => void;
+
+  setBalance: (data: bigint | null) => void;
+}
+
+// Provider hook that creates auth object and handles state
+export function useProvideAuth(): AuthContext {
+  const [wallet, setWallet] = useState<WalletInterface | undefined>();
+
+  const [principal, setPrincipal] = useState<Principal | undefined>(undefined);
+  const [agent, setAgent] = useState<HttpAgent | undefined>(undefined);
+
+  const [display, setDisplay] = useState(false);
+
+  const [balance, setBalance] = useState<bigint | null>(null);
+
+  const useInternetIdentity = function () {
+    const wlt = internetIdentity();
+    setWallet(wlt);
+    setDisplay(false);
+  };
+
+  //Displays modal to select wallet
+  const showModal = function (show: boolean) {
+    setDisplay(show);
+  };
+
+  function get() {
+    return {
+      showModal,
+      isShow: display,
+      setPrincipal,
+      principal: principal,
+      setAgent,
+
+      agent: agent,
+      balance,
+
+      wallet,
+      useInternetIdentity,
+      setBalance,
+    };
+  }
+
+  return get();
+}
+
+const authContext = createContext<AuthContext>(null!);
+export let auth: AuthContext;
+
+export function ProvideAuth({ children }: { children: ReactNode }) {
+  auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
---
+++ apps/fafty/src/utils/wallet/il.ts
@@ -0,0 +1,131 @@
+import { Actor, HttpAgent, Identity } from '@dfinity/agent';
+import { AuthClient } from '@dfinity/auth-client';
+import { auth } from '../../auth';
+
+const DFX_NETWORK = process.env.REACT_APP_DFX_NETWORK || 'ic';
+
+const dfxConfig = {
+  canisters: {
+    icpunks: {
+      build: './service/build.sh',
+      candid: 'service/icpunks/icpunks.did',
+      wasm: 'service/target/wasm32-unknown-unknown/release/icpunks.wasm',
+      type: 'custom',
+    },
+    ledger_proxy: {
+      main: 'service/motoko/ledger_proxy.mo',
+      type: 'motoko',
+    },
+    icpunks_storage: {
+      main: 'service/motoko/storage.mo',
+      type: 'motoko',
+    },
+    icpunks_claim: {
+      main: 'service/motoko/claim.mo',
+      type: 'motoko',
+    },
+    icpunks_assets: {
+      dependencies: ['icpunks'],
+      frontend: {
+        entrypoint: 'src/index.tsx',
+      },
+      source: ['build'],
+      type: 'assets',
+    },
+  },
+  dfx: '0.7.2',
+  networks: {
+    ic: {
+      providers: ['https://boundary.ic0.app/'],
+      type: 'persistent',
+    },
+    local: {
+      bind: '127.0.0.1:8000',
+      type: 'ephemeral',
+    },
+  },
+  version: 1,
+};
+
+export function getHost() {
+  if (DFX_NETWORK === 'ic') return dfxConfig.networks.ic.providers[0];
+
+  return dfxConfig.networks.local.bind;
+}
+
+export interface WalletInterface {
+  name: string;
+
+  logIn: () => void;
+  logOut: () => void;
+
+  requestTransfer: (data: any) => any;
+
+  getActor: <Type>(canisterId: string, idl: any) => Promise<Type | undefined>;
+
+  getBalance: () => any;
+}
+
+const IDENTITY_URL = 'https://identity.ic0.app';
+
+export default function internetIdentity() {
+  let authClient: AuthClient | null = null;
+  let identity: Identity | null = null;
+
+  async function getActor<Type>(
+    canisterId: string,
+    idl: any
+  ): Promise<Type | undefined> {
+    const agent = auth.agent;
+
+    const actor = Actor.createActor<Type>(idl, {
+      agent,
+      canisterId: canisterId,
+    });
+
+    return actor;
+  }
+
+  async function logIn() {
+    if (authClient == null) authClient = await AuthClient.create();
+
+    if (authClient != null) {
+      const host = getHost();
+
+      authClient.login({
+        identityProvider: IDENTITY_URL,
+        onSuccess: () => {
+          const client = authClient as AuthClient;
+          identity = client.getIdentity();
+
+          const agent = new HttpAgent({ host, identity });
+          auth.setAgent(agent);
+
+          auth.setPrincipal(identity.getPrincipal());
+
+          //   getBalance();
+
+          console.log('Logged in with II');
+        },
+        onError: (str) => {
+          console.log('Error while logging with II: ' + str);
+        },
+      });
+    }
+  }
+
+  function logOut() {
+    authClient?.logout();
+    auth.setAgent(undefined);
+    auth.setPrincipal(undefined);
+  }
+
+  return {
+    name: 'ii',
+    logIn,
+    logOut,
+    getActor,
+    // requestTransfer,
+    // getBalance,
+  };
+}
